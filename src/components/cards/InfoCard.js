a0383402abf3ae54d93bd81774e39642b4eef0ac
Neha Sharma <97179443+nehabhardwaj3001@users.noreply.github.com>
2023-04-20 14:50:51 +0530

    hover effects added for info cards

--- src/components/cards/InfoCard.js
+++ src/components/cards/InfoCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Typography, Col, Image } from "antd";
 import {
   CustomCard,
@@ -19,9 +19,24 @@ const InfoCard = ({
   background,
   color,
 }) => {
+  const [hovered, setHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setHovered(false);
+  };
+
   return (
     <Col span={12}>
-      <CustomCard background={background}>
+      <CustomCard
+        background={background}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        style={{ boxShadow: hovered ? "0 2px 8px rgba(0, 0, 0, 0.15)" : "none" }}
+      >
         <CardUpperSection color={color}>
           <Image src={image} preview={false} className="card-img" />
           <div>
