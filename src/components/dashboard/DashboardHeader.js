db08388173030745a97b06d461cb5ffa6d1d62aa
Neha Sharma <97179443+nehabhardwaj3001@users.noreply.github.com>
2023-04-20 13:59:31 +0530

    hover effects added in header

--- src/components/dashboard/DashboardHeader.js
+++ src/components/dashboard/DashboardHeader.js
@@ -11,8 +11,16 @@ import {
 } from "./StyledComponent";
 import { Typography, Image } from "antd";
 import { CaretDownOutlined } from "@ant-design/icons";
+import styled from "styled-components";
 
 const { Title } = Typography;
+
+const ScaledImage = styled(Image)`
+  transition: transform 0.2s ease-in-out;
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
 
 const DashboardHeader = () => {
   return (
@@ -20,14 +28,14 @@ const DashboardHeader = () => {
       <DashboardHeaderWrap>
         <RowAlignedContent>
           <LogoTitleWrap>
-            <Image src="/img/logo.svg" preview={false} />
+            <ScaledImage src="/img/logo.svg" preview={false} />
             <Title level={4}>Boro team</Title>
             <CaretDownOutlined />
           </LogoTitleWrap>
-          <Image src="/img/add-icon.svg" preview={false} />
+          <ScaledImage src="/img/add-icon.svg" preview={false} />
         </RowAlignedContent>
         <UserInfoWrap>
-          <Image src="/img/user.svg" preview={false} />
+          <ScaledImage src="/img/user.svg" preview={false} />
           <Title level={4}>Zahra hasht..</Title>
           <CaretDownOutlined />
         </UserInfoWrap>
